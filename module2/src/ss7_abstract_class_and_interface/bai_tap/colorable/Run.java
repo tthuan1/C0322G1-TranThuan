ff32d9e529689b6fb79aa5855c20d9fe6c8b1b14
tthuan1 <tranthuan1809@gmail.com>
2022-05-15 15:01:48 +0700

    15/5

--- module2/src/ss7_abstract_class_and_interface/bai_tap/colorable/Run.java
+++ module2/src/ss7_abstract_class_and_interface/bai_tap/colorable/Run.java
@@ -7,28 +7,18 @@ import ss7_abstract_class_and_interface.bai_tap.colorable.model.Shape;
 
 public class Run {
     public static void main(String[] args) {
-        Circle circle = new Circle();
-        Rectangle rectangle = new Rectangle();
-        Square square=new Square();
-        Shape[] shapes = new Shape[3];
+        Shape[] shapes = new Shape[4];
         shapes[0] = new Circle(3.5, "indigo", false);
-        shapes[1] = new Rectangle(2.5, 3.8, "orange", true);
-        shapes[2] = new Square(2.5, "orange", true);
+        shapes[1] = new Square(6.7, "orange", true);
 
-//        for (Shape shape : shapes) {
-//            if (shape instanceof Square) {
-//                System.out.println(shape);
-//                square.howToColor();
-//            }
-//            if (shape instanceof Circle) {
-//                System.out.println(shape);
-//            }
-//            if (shape instanceof Rectangle) {
-//                System.out.println(shape);
-//            }
-//        }
-        for (int i = 0; i < shapes.length; i++) {
-            System.out.println("Diện tích của các hình là: "+shapes[i]);
+        shapes[2] = new Rectangle(2.5, 3.8, "orange", true);
+        shapes[3] = new Square(2.5, "orange", true);
+
+        for (Shape shape : shapes) {
+            System.out.println("Diện tích và độ dài của các hình là: " + shape);
+            if (shape instanceof Square) {
+                ((Square) shape).howToColor();
+            }
         }
     }
 }
