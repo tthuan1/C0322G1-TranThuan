a0f96c7c916e47167fee14504a0af4ac711f01a7
tthuan1 <tranthuan1809@gmail.com>
2022-05-19 17:17:20 +0700

    19/5

--- module2/src/ss11_stack_queue/exercise/Palindrome.java
+++ module2/src/ss11_stack_queue/exercise/Palindrome.java
@@ -1,9 +1,28 @@
 package ss11_stack_queue.exercise;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Palindrome {
-    String str="Palindrome";
-    String[] arrStr = str.split("");
-//    Queue queue=new
+    static Queue<String> queue = new LinkedList<>();
+
+    public static void main(String[] args) {
+        String str = "Able was I ere I saw Elba1";
+        String newStr = str.toLowerCase();
+        String[] arrStr = newStr.split("");
+        queue.addAll(Arrays.asList(arrStr));
+
+        boolean check = true;
+        for (int i = arrStr.length - 1; i > 0; i--) {
+            if (!queue.poll().equals(arrStr[i])) {
+                check = false;
+            }
+        }
+        if (check) {
+            System.out.println("ok");
+        } else {
+            System.out.println("sai");
+        }
+    }
 }
