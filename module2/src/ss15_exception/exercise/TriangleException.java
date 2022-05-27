3397491649b4acc6c33161df081aa845a2fc6352
tthuan1 <tranthuan1809@gmail.com>
2022-05-27 08:21:57 +0700

    27/5

--- module2/src/ss15_exception/exercise/TriangleException.java
+++
@@ -1,9 +0,0 @@
-package ss15_exception.exercise;
-
-public class TriangleException extends Exception {
-    public TriangleException() {
-    }
-    public TriangleException(String message) {
-        super(message);
-    }
-}
