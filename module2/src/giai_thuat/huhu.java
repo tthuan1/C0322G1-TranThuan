14ca14c67f0e39cb7a052e684fee7172b4187514
tthuan1 <tranthuan1809@gmail.com>
2022-06-22 20:49:25 +0700

    22/6

--- module2/src/giai_thuat/huhu.java
+++ module2/src/giai_thuat/huhu.java
@@ -1,33 +1,60 @@
 package giai_thuat;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class huhu {
 
     public static void main(String[] args) {
-        int n = 10;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i < n / 2) {
-                    if (j >= i && j < (n - i)) {
-                        System.out.print(" * ");
-                    } else {
-                        System.out.print(" - ");
-                    }
-                } else {
-                    if (j <= i && j >= (n-1 - i)) {
-                        System.out.print(" * ");
-                    } else {
-                        System.out.print(" - ");
-                    }
+//        int n = 10;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (i < n / 2) {
+//                    if (j >= i && j < (n - i)) {
+//                        System.out.print(" * ");
+//                    } else {
+//                        System.out.print("   ");
+//                    }
+//                } else {
+//                    if (j <= i && j >= (n-1 - i)) {
+//                        System.out.print(" * ");
+//                    } else {
+//                        System.out.print("   ");
+//                    }
+//                }
+//            }
+//            System.out.println();
+//        }
+
+        test("abcbdcef");
+    }
+    public static void test(String s){
+        String[] hi= s.split("");
+        String[] temp=new String[hi.length];
+        String a;
+        for (int i = 0; i < hi.length; i++) {
+             a= hi[i];
+            for (int j = i+1; j < hi.length; j++) {
+                if (a.equals( hi[j])){
+                    temp[j]=a;
                 }
             }
-            System.out.println();
         }
+        int length=0;
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i] !=null){
+                length++;
+            }
+        }
+        String[] ok= new String[length];
+        int j=0;
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i] !=null){
+                ok[j]=temp[i];
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(ok));
+
     }
 
 
