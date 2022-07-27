5adf7a7242d48d98e7ad818c3a77a796739ed20d
tthuan1 <tranthuan1809@gmail.com>
2022-07-27 08:30:27 +0700

    27/07

---
+++ module4/ss9-AOP/library_app/src/main/java/com/example/config/ExceptionHandle.java
@@ -0,0 +1,12 @@
+package com.example.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Component
+public class ExceptionHandle {
+    @ExceptionHandler(Exception.class)
+    private String showErrorPage(){
+        return "error";
+    }
+}
