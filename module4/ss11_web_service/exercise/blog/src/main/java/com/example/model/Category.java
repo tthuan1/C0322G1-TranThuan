d890d9e27f5f89ce99a1e1aa36e4ce18fea5a709
tthuan1 <tranthuan1809@gmail.com>
2022-07-28 22:37:00 +0700

    28/07

---
+++ module4/ss11_web_service/exercise/blog/src/main/java/com/example/model/Category.java
@@ -0,0 +1,51 @@
+package com.example.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+//    @JsonManagedReference
+    private Set<Blog> blog;
+
+    public Category() {
+    }
+
+    public Category(Integer id, String name, Set<Blog> blog) {
+        this.id = id;
+        this.name = name;
+        this.blog = blog;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Blog> getBlog() {
+        return blog;
+    }
+
+    public void setBlog(Set<Blog> blog) {
+        this.blog = blog;
+    }
+}
