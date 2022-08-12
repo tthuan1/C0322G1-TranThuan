610db4034d8f08b8b55386aa37d49a135bcf1851
tthuan1 <tranthuan1809@gmail.com>
2022-08-12 15:11:58 +0700

    12/8

---
+++ module5/ss2/practice/typescript-variable/src/index.ts
@@ -0,0 +1,9 @@
+// happy coding 👻
+console.log("hello world");
+console.log("hello con cặc");
+let width: number;
+let height: number;
+width = 10.5;
+height = 20;
+let area: number = width*height;
+console.log(`Diện tích hình chữ nhật: ${area}`);
