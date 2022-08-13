2287c6c4501ce5720bfeb8777c3c88856748ccff
tthuan1 <tranthuan1809@gmail.com>
2022-08-13 13:51:41 +0700

    13/8 fix comment

--- module5/ss2_typescript/exercise/fibonacci/src/index.ts
+++ module5/ss2_typescript/exercise/fibonacci/src/index.ts
@@ -1,23 +1,24 @@
 // happy coding 👻
-function fibonnacci(n: number): number {
-    if (n == 0 || n == 1) {
-        return 1;
-    } else {
-        return fibonnacci(n - 2) + fibonnacci(n - 1);
-    }
+function fibonacci(n: number): number {
+  if (n == 0 || n == 1) {
+    return 0;
+  } else if (n == 2 || n == 3) {
+    return 1;
+  } else {
+    return fibonacci(n - 2) + fibonacci(n - 1);
+  }
 }
 
-function display() {
-    // @ts-ignore
-    let m = document.getElementById("num").value;
-    let fibonnacciList = 0;
-    for (let i = 0; i < m; i++) {
-        console.log("Số thứ "+(i+1)+" là: "+fibonnacci(i));
-        fibonnacciList += fibonnacci(i);
-        console.log(fibonnacci(1));
-    }
-    // @ts-ignore
-    document.getElementById("ketQua").innerHTML="Tổng các số fibonnacci là: "+ fibonnacciList;
+function displayTotal() {
+  // @ts-ignore
+  let m = document.getElementById("num").value;
+  let totalFibonacci = 0;
+  for (let i = 1; i <= m; i++) {
+    console.log("Số thứ " + i + " là: " + fibonacci(i));
+    totalFibonacci += fibonacci(i);
+  }
+  // @ts-ignore
+  document.getElementById("ketQua").innerHTML = "Tổng các số fibonnacci là: " + totalFibonacci;
 }
 
 
